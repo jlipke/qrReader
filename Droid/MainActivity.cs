2773ba22c2f2f8cb1d735459995e745283911e70
Joseph Lipke <59025411+jlipke@users.noreply.github.com>
2021-02-21 11:31:16 -0600

    Initial commit

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+//using ZXing.Net.Mobile.Forms;
+
+
+
+namespace qrReader2.Droid
+{
+    [Activity(Label = "qrReader2.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication( new qrReader2.App());
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
+    }
+
+
+}
---
+++ qrReader2/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace qrReader2
+{
+    public partial class qrReader2Page : ContentPage
+    {
+        public qrReader2Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            Navigation.PushAsync(scan);
+            scan.OnScanResult += (result) =>
+              {
+                  Device.BeginInvokeOnMainThread(async () =>
+                  {
+                      await Navigation.PopAsync();
+                      mycode.Text = result.Text;
+
+                  });
+
+              };
+        }
+    }
+
+    }
+
